00c3f28c33fa81ee9bee3046695590c4ee5d0eac
ckrac <kennethracadio@hotmail.com>
2018-01-18 17:09:00 +0000

    Refactor function as for each

--- callbackArguments.js
+++ callbackArguments.js
@@ -1,16 +1,38 @@
-// Exercise - Callback Arguments
+// /////// Exercise - Callback Arguments
 
-// The second argument/parameter is expected to be a function
+// // The second argument/parameter is expected to be a function
+// function findWaldo(arr, found) {
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] === "Waldo") {
+//       found(i);   // execute callback
+//     }
+//   }
+// }
+
+// function actionWhenFound(index) {
+//   console.log("Found Waldo at index " + index + "!");
+// }
+
+// findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+///// Exercise - Array forEach
+
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+  arr.forEach(function (element, index) {
+    if (element === "Waldo") {
+      // console.log("hey" + element[index]);
+      found(index);
     }
-  }
+  })
+};
+
+function actionWhenFound(pos) {
+  console.log("Found Waldo at index " + pos + "!");
 }
 
-function actionWhenFound(index) {
-  console.log("Found Waldo at index " + index + "!");
-}
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+
